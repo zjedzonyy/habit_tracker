b9c55362f067b96a2dc7ee376e32629a9a1a2120
zjedzonyy <artompor@gmail.com>
2024-08-20 16:52:51 +0200

    Create basic UI with HTML and CSS + define Task class in createHabit.js

---
+++ src/createHabit.js
@@ -0,0 +1,40 @@
+// logic for creating habit
+
+// habit musi miec: 
+// NAME,
+// CZESTOTLIWOSC,
+// WAZNOSC ZADANIA,
+// O JAKIEJ PORZE?,
+// WYKONANE?
+
+class Habit {
+    constructor(name, frequency, importance, timeOfDay, finished) {
+        this.name = name;
+        this.frequency = frequency;
+        this.importance = importance;
+        this.timeOfDay = timeOfDay;
+        this.finished = finished;
+    }
+}
+
+
+
+export default function createHabit(name, importance) {
+    function describe() {
+        return `Nazwa: ${this.name}, Importance: ${this.importance}`;
+    }
+
+    return {
+        name,
+        importance,
+        describe
+    };
+}
+
+// function () {
+
+// }
+
+// function without() {
+
+// }
--- src/index.js
+++ src/index.js
@@ -1,58 +1,12 @@
 // index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
-import loadHomePage from "./loadHomePage.js";
-import loadMenuPage from "./loadMenuPage.js";
-import loadAboutPage from "./loadAboutPage.js";
+import createHabit from "./createHabit.js";
 
-function addListenersToButtons() {
-    const home = document.getElementById("home");
-    const menu = document.getElementById("menu");
-    const about = document.getElementById("about");
-    const content = document.getElementById("content");
-    const myGithub = document.getElementById("github");
-    const buttons = document.querySelectorAll('nav button');
-    // remove html and style for the content div
-    function cleanContent() {
-        const styleSheet = document.getElementById("dynamicStyles");
-        if (styleSheet) {
-            styleSheet.remove();
-        }
-        content.innerHTML = "";
-    }
+// kreator habitow
+// dodawanie habitow do routines
+// importowanie habitow i rutyn do kalendarza
 
-    // display active button
-    function setActiveButton(clickedButton) {
-        buttons.forEach(button => {
-            button.classList.remove('button-active');
-        });
-        clickedButton.classList.add('button-active');
-    }
+const habit = createHabit("Swim", "Often");
 
-    console.log(home);
-    home.addEventListener("click", () => {
-        setActiveButton(home);
-        cleanContent();
-        loadHomePage();
-    })
-
-    menu.addEventListener("click", () => {
-        setActiveButton(menu);
-        cleanContent();
-        loadMenuPage();
-    })
-
-    about.addEventListener("click", () => {
-        setActiveButton(about);
-        cleanContent();
-        loadAboutPage();
-    })
-
-    myGithub.addEventListener("click", () => {
-        window.location.href = 'https://github.com/zjedzonyy';
-    })
-}
-
-
-addListenersToButtons();
-loadHomePage();
+console.log(habit.describe());
